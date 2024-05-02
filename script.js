23b8cc6035b6d608817f99cb5f9327cafd3f3f2c
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-02 14:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function submitForm() {
+  const name = document.getElementById('name').value;
+  const age = parseInt(document.getElementById('age').value, 10);
+  
+  // if (name.trim() === "" || isNaN(age)) {
+  //   alert("Both name and age fields must be filled out.");
+  //   return;
+  // }
+
+  ageValidationPromise(name, age).then(message => {
+    alert(message);
+  }).catch(message => {
+    alert(message);
+  });
+}
+
+function ageValidationPromise(name, age) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry, ${name}. You aren't old enough.`);
+      }
+    }, 4000); // wait for 4 seconds
+  });
+}
+
